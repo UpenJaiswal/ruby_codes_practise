2dabc8641b0eb130956890de4336b9d9deed1f9f
Upen Jaiswal <35937245+smoochkiller@users.noreply.github.com>
2018-04-03 14:51:01 +0530

    Add files via upload

--- odd_even.rb
+++ odd_even.rb
@@ -1,36 +1,9 @@
 
 
-#  puts "Input ur number"
-#  number = gets.chomp.to_i
-#  if number.even?
-#    puts "Number is even"
-#  else
-#    puts "Number is odd"
-#  end
-
-#name = "upen"
-#p name.include?("y")
-#p name.include?("p")
-
-
-
-
-#puts "enter ur letter"
-#letter = gets.chomp()
-#if letter = upen.include?()
-# puts "word contains"
-#else
-#  puts "word does not contains"
-  #end
-
-
-#puts "Enter ur password"
-#password = gets.chomp()
-
-#if password.length <= 7
-#  puts "Password is invalid"
-#elsif password.length == 8
-#  puts "Password is weak"
-#elsif password.length >= 9
-#  puts "Password is strong"
-#end
+  puts "Input ur number"
+  number = gets.chomp.to_i
+  if number.even?
+    puts "Number is even"
+  else
+    puts "Number is odd"
+  end
